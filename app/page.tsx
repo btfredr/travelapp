94e3c092b04cc6aa3c0679390897bd9893552af1
Fredrik <fredrikf2000@gmail.com>
2023-10-30 20:34:15 +0100

    started adding components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-      <h1 className="text-bold text-teal-500">
+      <h1 className="text-bold text-teal-500 text-xl">
         Hello world!
       </h1>
   )
