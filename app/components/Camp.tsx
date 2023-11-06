86f5a0055cf9e72261feecba29c84d83a72d874a
Fredrik <fredrikf2000@gmail.com>
2023-11-06 20:44:49 +0100

    added content to product cards

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -1,3 +1,6 @@
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
+
 interface CampProps {
     backgroundImage: string;
     title: string;
@@ -6,6 +9,29 @@ interface CampProps {
 }
 
 const CampSite = ({backgroundImage, title, subtitle, peopleJoined}) => {
+    return (
+        <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+            <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:placeholder-gray-100">
+                <div className="flexCenter gap-4">
+                    <div className="rounded-full bg-green-50 p-4">
+                        <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h4 className="bold-18 text-white">{title}</h4>
+                        <p className="regular-14 text-white">{subtitle}</p>
+                    </div>
+                </div>
+                <div className="flexCenter gap-6">
+                    <span className="flex -space-x-4 overflow-hidden">
+                        {PEOPLE_URL.map((url) => (
+                            <Image className="inline-block h-10 w-10 rounded-full" src={url} key={url} alt="person" width={52} height={52} />
+                        ))}
+                    </span>
+                    <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+                </div>
+            </div>
+        </div>
+    )
     
 }
 
@@ -16,10 +42,18 @@ const Camp = () => {
             <CampSite 
                 backgroundImage="bg-bg-img-1"
                 title="Putuk Truno Camp"
-                subtitle="Banyuwangi, Indonesia"
+                subtitle="Prigen, Pasuruan"
                 peopleJoined="50+ Joined"
             />
-            <CampSite />
+            <CampSite 
+                backgroundImage="bg-bg-img-2"
+                title="Mountain View Camp"
+                subtitle="Somewhere in the wild"
+                peopleJoined="50+ Joined"
+            />
+        </div>
+        <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+            .
         </div>
     </section>
   )
