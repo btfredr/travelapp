c695ca51109bc537b68aaa745a9d6f98690afbb2
Fredrik <fredrikf2000@gmail.com>
2023-11-06 20:27:36 +0100

    working on camp site component

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -1,4 +1,11 @@
-const CampSite = () => {
+interface CampProps {
+    backgroundImage: string;
+    title: string;
+    subtitle: string;
+    peopleJoined: string;
+}
+
+const CampSite = ({backgroundImage, title, subtitle, peopleJoined}) => {
     
 }
 
@@ -6,7 +13,12 @@ const Camp = () => {
   return (
     <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
         <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-            <CampSite />
+            <CampSite 
+                backgroundImage="bg-bg-img-1"
+                title="Putuk Truno Camp"
+                subtitle="Banyuwangi, Indonesia"
+                peopleJoined="50+ Joined"
+            />
             <CampSite />
         </div>
     </section>
